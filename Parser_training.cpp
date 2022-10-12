625dc36f9795e38e46956ebe0022f07aa14a1c89
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-10-12 20:07:05 +0300

    Парсер может обрабатывать более одной пары Без наличия признака новой пары, все символы после последней буквы записываются в значение текущей пары Значения пар с пробелами и иными символами заключаются в кавычки

--- Parser_training.cpp
+++ Parser_training.cpp
@@ -15,7 +15,7 @@ using namespace std;
 //        
 //}
 
-void ParseParams1(const string& params)
+std::map<string, string> ParseParams1(const string& params)
 {
     // на вход дается строка вида "/param value"
     // параметр определяется по слэшу в начале, чтение до первого пробела
@@ -35,35 +35,58 @@ void ParseParams1(const string& params)
     {
         short int name_pos_begin{ pos };
         short int name_pos_end{ name_pos_begin };
+
+        // ищем границы первого слова и записываем его
         while (params[pos++] != ' ')
             name_pos_end++;
-
         for (int i{ name_pos_begin }; i <= name_pos_end; i++)
             param_name.push_back(params[i]);
 
         short int value_pos_begin{ pos };
         short int value_pos_end{ value_pos_begin };
-        bool space_incountered{ false };
-        while (params[pos++] && space_incountered==false)
+        bool new_pair_incounted{ false };
+        bool value_with_spaces{ false };
+
+        // ищем границы второго слова и записываем его
+        while (params[pos++] && new_pair_incounted==false)
             if (params[pos] != ' ')
                 value_pos_end++;
             else
-                space_incountered = true;
+                if(params[pos])
+                    // это не последняя пара параметр-значение
+                    if (params[pos] == ' ' && params[pos + 1] == '/' && isalpha(params[pos+2]) )
+                    {
+                        new_pair_incounted = true;
+                        pos++;
+                    }
+                    // пробел относился к значению текущей пары
+                    else
+                    {
+                        value_with_spaces = true;
+                        value_pos_end++;
+                    }
+
+        // значения с пробелами должны быть в кавычках
+        if (value_with_spaces)
+            param_value.push_back('"');
         for (int i{ value_pos_begin }; i <= value_pos_end; i++)
             param_value.push_back(params[i]);
+        if (value_with_spaces)
+            param_value.push_back('"');
 
         pairs.insert({ param_name, param_value });
 
         param_name.clear();
         param_value.clear();
 
-        if(!space_incountered)
+        if(!new_pair_incounted)
             pos--;
     }
     for (auto itr{ pairs.begin() }; itr != pairs.end(); ++itr) {
         cout << itr->first
             << '\t' << itr->second << '\n';
     }
+    return pairs;
 }
 
 //от начала до первого пробела - имя первой пары
@@ -73,7 +96,14 @@ void ParseParams1(const string& params)
 
 int main()
 {
-    string str = "/param1 value1 param2 value2 param3 value3 param4 value4";
+    string str = "/param1 value  /  1 /param2 value2 /param3 value3 /param4 value4";
+    // Значения параметров, содержащие пробелы, необходимо заключать в кавычки или экранировать с помощью обратного слеша:
+    // value   1 -> "value   1" || value\\\1
+    //value    1
+
+    //
     ParseParams1(str);
     return 0;
 }
+
+// TODO: выкидывать исключения из проекта тестового задания,    опционально: экранирование пробелов, прямых и обратных слешей, кавычек
